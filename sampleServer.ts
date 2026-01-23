44882691234af83b1ef1cf80af10669bd15c69d9
wan ka chiu <wankachiu@gmail.com>
2026-01-23 13:00:00 -0500

    sampleServer.ts

---
+++ sampleServer.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/testAPI", (req: Request, res: Response) => {
+    const testResponse = {
+        testField1: "Test 1",
+        testField2: "Test2",
+        testNumber: 3,
+    }
+  res.json(testResponse);
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
